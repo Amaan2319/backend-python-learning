866c3b6d08cf27251d8f4459709893d9798de501
Amaan <amanabbassaiyed@gmail.com>
2026-07-10 00:26:43 +0530

    DSA best time to buy stock

--- dsa.py
+++ dsa.py
@@ -10,3 +10,15 @@ def twoSum(arr,target):
             seen[num]=i
 
 print(twoSum([2,7,1,3,9],9))
+
+
+# best time to buy and sell stock
+def maxProfit(self, prices: List[int]) -> int:
+        min_price =float('inf')
+        max_profit=0
+        for price in prices:
+            if price<min_price:
+                min_price=price
+            elif price-min_price>max_profit:
+                max_profit=price-max_profit   
+        return max_profit

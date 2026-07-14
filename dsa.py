5d8cd03c814c98e438563e9a629aadfde9f4a155
Amaan <amanabbassaiyed@gmail.com>
2026-07-14 09:04:38 +0530

    parameterised Decorators

--- dsa.py
+++ dsa.py
@@ -143,3 +143,20 @@ def test():
     print("test function")
 
 test()
+
+# parameterized decorator
+def repeat(num=2):
+    def decorator_repeat(func):
+        def wrapper(*args,**kwargs):
+            result = None
+            for _ in range(num):
+                result = func(*args,**kwargs)
+            return result
+        return wrapper
+    return decorator_repeat
+
+@repeat(num=3)
+def greet(name):
+    return f"Kem chho {name}"
+kamlesh=greet("kalpesh")
+print(kamlesh)

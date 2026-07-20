e80ed102ea0fc9da7287c0b06d7d46529c42cc9e
Amaan <amanabbassaiyed@gmail.com>
2026-07-20 22:27:12 +0530

    valid anagram check

--- dsa.py
+++ dsa.py
@@ -144,3 +144,12 @@ def test():
 
 test()
 
+# anagram program
+def isAnagram(s,t):
+    if len(s)!=len(t):
+        return False
+    count_s, count_t = {},{}
+    for i in range(len(s)):
+        count_s[s[i]] = 1 + count_s.get(s[i],0)
+        count_t[t[i]] = 1 + count_t.get(t[i],0)
+    return count_t == count_s
